4c4e837be1374804e49b6f3301411d788ad88cd0
Dmitry <manxxeta@gmail.com>
2018-07-31 08:38:09 -0700

    added a function to render data to one text

--- edit-note.js
+++ edit-note.js
@@ -6,63 +6,37 @@ var notepadDivElement = document.getElementsByClassName('notepad')[0];
 /*-----------------------------------------------------------------*/
 /*-----------------defining all the functions----------------------*/
 /*-----------------------------------------------------------------*/
-function renderData()
-{
-  // need to clear physical elements on a page and rerender them again with new data
-  clearElementsOnPage();
 
-  // find ul on a page
-  var ulEl = document.getElementById('notesUl');
 
-  // show size of local storage
-  var entriesSize = localStorage.length;
+// // function to clear data from a local storage
+// function clearData()
+// {
+//   // clear physical elements
+//   clearElementsOnPage();
 
-  // running through each entry
-  for (var i=0 ; i < entriesSize; i++)
-  {
-  // for each entry creating a temporary key
-    var tmpKey = 'backnote' + i;
-    // and recieving a temporary data
-    var tmpData = localStorage.getItem(tmpKey);
-
-    // creating a list element and attaching it data to it
-    var liEl = document.createElement('li');
-    var text = document.createTextNode(tmpData);
-    liEl.appendChild(text);
-    ulEl.appendChild(liEl);
-  }
-
-}
-
-// function to clear data from a local storage
-function clearData()
-{
-  // clear physical elements
-  clearElementsOnPage();
-
-  // clear storage
-  localStorage.clear();
+//   // clear storage
+//   localStorage.clear();
 
   
 
-}
+// }
 
 
 
 
-// function to clear html elements on a page
-function clearElementsOnPage()
-{
-  var ulElement = document.getElementById('notesUl');
+// // function to clear html elements on a page
+// function clearElementsOnPage()
+// {
+//   var ulElement = document.getElementById('notesUl');
 
-  while(ulElement.firstChild)
-  {
-    ulElement.removeChild(ulElement.firstChild);
-  }
+//   while(ulElement.firstChild)
+//   {
+//     ulElement.removeChild(ulElement.firstChild);
+//   }
 
-  // // reload last tab
-  // browser.tabs.reload();
-}
+//   // // reload last tab
+//   // browser.tabs.reload();
+// }
 
 //TODO: make a function that collect all data from a local storage and return a big text
 function renderDataToText()
@@ -92,7 +66,7 @@ function renderDataToText()
 //TODO: make a function that pull all that data into a notepad class element
 function showOnNotepad()
 {
-
+  notepadDivElement.innerHTML = renderDataToText();
 }
 
 
