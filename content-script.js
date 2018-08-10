64e2309e915594ad508848ee4d33942d95fa6b23
Dmitry <manxxeta@gmail.com>
2018-08-10 15:06:40 -0700

    firefox edition final 1.5

--- content-script.js
+++ content-script.js
@@ -1,17 +1,30 @@
 
-//---------------------------------------------------------------------
-// copy the selected content to clipboard and a local storage
-function copySelection() 
+// //---------------------------------------------------------------------
+// // copy the selected content to clipboard and a local storage
+// function copySelection() 
+// {
+//   var selectedText = window.getSelection().toString().trim();
+
+//   // if content is not empty - then copy it
+//   if (selectedText) 
+//   {
+//     document.execCommand('Copy');
+//   }
+// }
+
+// //---------------------------------------------------------------------
+// // Add copySelection() as a listener to mouseup events.
+// document.addEventListener('mouseup', copySelection);
+
+// send data to a backnote
+function sendTextToBacknote()
 {
+  // capture selected text 
   var selectedText = window.getSelection().toString().trim();
 
-  // if content is not empty - then copy it
-  if (selectedText) 
-  {
-    document.execCommand('Copy');
-  }
+  // send it to edit-note.js with key : backnote_data
+  browser.runtime.sendMessage({backnoteData: selectedText}, function(){ console.log('DATA SENT!');});
 }
 
-//---------------------------------------------------------------------
 // Add copySelection() as a listener to mouseup events.
-document.addEventListener('mouseup', copySelection);
+document.addEventListener('mouseup', sendTextToBacknote);
--- edit-note.js
+++ edit-note.js
@@ -3,75 +3,12 @@ var notepadDivElement = document.getElementsByClassName('notepad')[0];
 var downloadButton = document.getElementById('downloadButton');
 
 /*-----------------------------------------------------------------*/
+// previous paste to prevent duplicates
+var previousPaste = '';
+
+/*-----------------------------------------------------------------*/
 // first line just an empty one
 notepadDivElement.innerText += '\n';
-
-/*-----------------------------------------------------------------*/
-// set an string to a clipboard
-function setStringToClipboard(str)
-{
-  // creating an <textarea> element
-  var el = document.createElement('textarea');
-  // setting it's value to a string we will set to a clipboard
-  el.value = str;
-  // make it read-only (optional, but just to make sure)
-  el.setAttribute('readonly', '');
-  // moving it somewhere outside of screen so it will be invisible
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  // attaching it to a body
-  document.body.appendChild(el);
-  // checking if there is anything selected already, true - save it, false - not saving it (later)
-  var selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
-  // selecting a content inside of our <textarea>
-  el.select();
-  // copying it
-  document.execCommand('copy');
-  // removing our <textarea> element
-  document.body.removeChild(el);
-  // checking if there been some content selected before
-  if (selected)
-  {
-    // unselecting everything on a page
-    document.getSelection().removeAllRanges();
-    // restore an original selection
-    document.getSelection().addRange(selected);
-  }
-}
-
-/*-----------------------------------------------------------------*/
-// paste a data
-function pasteData()
-{
-  notepadDivElement.focus();
-  document.execCommand('Paste');
-}
-
-/*-----------------------------------------------------------------*/
-// filtering a paste data
-const stopPasting = event =>
-{
-  // setting a data from clipboard
-  const data = event.clipboardData.getData('text');
-
-  // check if it's an empty space
-  if (data === ' ')
-  {
-    // if true, then do not paste
-    return event.preventDefault();
-  }
-  else
-  {
-    // paste data
-    notepadDivElement.innerText += data;
-
-    // Setting empty space to cb (so we can paste only once)
-    setStringToClipboard(' ');
-
-    // move caret to a new line two times
-    notepadDivElement.innerText += '\n\n';
-  }
-};
 
 /*-----------------------------------------------------------------*/
 // download note as backnote.txt
@@ -94,10 +31,23 @@ function downloadTxt()
 }
 
 /*-----------------------------------------------------------------*/
-// listeners:
-// on paste go through our filter
-document.addEventListener('paste', stopPasting);
-// render from a clipboard on a mouseover
-document.addEventListener('mouseover', pasteData);
+// recieving data with a key : backnoteData
+browser.runtime.onMessage.addListener(
+  function(request, sender, sendResponse)
+  {
+    // get a data
+    var data = request.backnoteData;
+    // check if previous one is not the same
+    if (data !== previousPaste)
+    {
+    // paste the data into the document.
+      notepadDivElement.innerText += data;
+      // add an empty line and move caret to new line
+      notepadDivElement.innerText += '\n\n';
+      // set previous
+      previousPaste = data;
+    }
+  });
+
 // adding a listener to downloadButton
 downloadButton.addEventListener('click', downloadTxt);
